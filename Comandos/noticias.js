1265180f9564493da15030a309ffa7fe4c49e0ce
Matheus Bruno <matheusferreirabruno@hotmail.com>
2023-04-11 21:47:43 -0300

    comando para buscar noticias por categoria

--- Comandos/noticias.js
+++ Comandos/noticias.js
@@ -1,18 +1,15 @@
-const Discord = require("discord.js")
+const {EmbedBuilder, ApplicationCommandType} = require("discord.js");
 const NewsAPI = require('newsapi');
-const config = require("./config.json")
-const newsapi = new NewsAPI(config.token_newsapi);
-
 
 module.exports = {
   name: "noticias",
   description: "Veja as últimas 10 notícias sobre um determionado assunto",
-  type: Discord.ApplicationCommandType.ChatInput,
+  type: ApplicationCommandType.ChatInput,
   options: [
     {
       name: 'categoria',
       description: 'A categoria das notícias que você deseja ver',
-      type: Discord.ApplicationCommandType.Message,
+      type: ApplicationCommandType.Message,
       required: false,
       choices: [
         {
@@ -32,7 +29,35 @@ module.exports = {
   ],
 
   run: async (client, interaction) => {
+    const config = require("./../config.json");
+    const newsapi = new NewsAPI(config.token_newsapi);
 
+    function createEmbed(news) {
+        let embeds = [];
+      
+        news.forEach(_new => {
+          const exampleEmbed = new EmbedBuilder()
+          .setColor(0x0099FF)
+          .setTitle(_new.source.name)
+          .setURL(_new.url)	
+          .setDescription(_new.title)	          
+          .setTimestamp(new Date(_new.publishedAt))
+          .setFooter({ text: _new.author});
+      
+          embeds.push(exampleEmbed);
+        });
+      
+        return embeds;
+    }
 
+    const response = await newsapi.v2.topHeadlines({
+        country: 'br',
+        category: `${interaction.options.getString('categoria')}`,
+        pageSize: 10
+    });
+  
+    const articles = response.articles;    
+    let embeds = createEmbed(articles);
+    interaction.reply({ embeds: embeds });
   }
 }
